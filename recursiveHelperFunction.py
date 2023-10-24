22626f0ad58d6e0fe8eeb819e9584ee4fcb837cf
Yasser Moussaddak <yassermoussaddak21@gmail.com>
2023-10-24 10:34:00 +0300

    some new concepts

---
+++ recursiveHelperFunction.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    # Base case: if n is 0 or 1, return 1
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case: n! = n * (n-1)!
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(4))
